d0a016fac148828bc81f15a15815262898d2c7fd
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-13 16:17:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculateBMI() {
+  let weight = parseInt(document.getElementById("weight").value);
+  let height = parseInt(document.getElementById("height").value);
+
+  console.log(weight, height);
+  document.querySelector("#result").innerHTML;
+  if (height === "") result.innerHTML = "Provide a valid Height!";
+  else if (weight === "") result.innerHTML = "Provide a valid Weight!";
+  else {
+    let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    result.innerHTML = "Your BMI is " + bmi;
+  }
+}
+
+function convertCelsius() {
+  let celsius = parseInt(document.getElementById("celsius").value);
+
+  console.log(celsius, fahrenheit);
+  document.querySelector("#answercelsius").innerHTML;
+  let ctemp = (celsius * (9 / 5) + 32).toFixed(0);
+  answerincelsius.innerHTML = ctemp + "F";
+}
+
+function convertFahrenheit() {
+  let fahrenheit = parseInt(document.getElementById("fahrenheit").value);
+  document.querySelector("#answerfahrenheit").innerHTML;
+  let ftemp = ((fahrenheit - 32) * (5 / 9)).toFixed(0);
+  answerinfahrenheit.innerHTML = ftemp + "C";
+}
