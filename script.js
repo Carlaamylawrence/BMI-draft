bc8d38b18683f2218e3cdd93312d049275b8ea38
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-15 10:58:11 +0200

    final stylings

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ function calculateBMI() {
 
   console.log(weight, height);
   document.querySelector("#result").innerHTML;
-  if (height === "") result.innerHTML = "Provide a valid Height!";
-  else if (weight === "") result.innerHTML = "Provide a valid Weight!";
+  if (height === "" || isNaN(height))
+    result.innerHTML = "Provide a valid Height!";
+  else if (weight === "" || isNaN(weight))
+    result.innerHTML = "Provide a valid Weight!";
   else {
     let bmi = parseInt(weight / ((height * height) / 10000));
-    result.innerHTML = "Your BMI is " + bmi;
+    if (bmi < 18.6)
+      result.innerHTML = `You are under weight: Your BMI is = ${bmi}`;
+    else if (bmi >= 18.6 && bmi < 24.9)
+      result.innerHTML = `You have a healthy weight: Your BMI is = ${bmi}`;
+    else result.innerHTML = ` You are considered over weight = ${bmi}`;
   }
 }
