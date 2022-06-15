55c1b56efd7aadf0370fe944000ab6bffea6f425
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-15 15:42:26 +0200

    result size

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ function calculateBMI() {
     let bmi = parseInt(weight / ((height * height) / 10000));
     if (bmi < 18.6)
       result.innerHTML = `You are under weight: Your BMI is = ${bmi}`;
-    else if (bmi >= 18.6 && bmi < 24.9)
+    else if (bmi >= 18.6)
       result.innerHTML = `You have a healthy weight: Your BMI is = ${bmi}`;
-    else result.innerHTML = ` You are considered over weight = ${bmi}`;
+    else if (bmi >= 24.9)
+      result.innerHTML = ` You are considered over weight = ${bmi}`;
+    else if (bmi >= 30);
+    result.innerHTML = ` You are considered obese = ${bmi}`;
   }
 }
